06448790dc5f26a59c65e979c37a2e3af0271e9f
André Luis de Souza <andrezera.dev@gmail.com>
2025-11-25 10:55:07 -0300

    leves ajustes na interface

--- src/components/chat/ChatInterface.tsx
+++ src/components/chat/ChatInterface.tsx
@@ -83,7 +83,9 @@ const ChatInterface: React.FC<ChatInterfaceProps> = ({
   const { tokens, hasUnlimitedTokens, canSendMessage, decrementToken, timeUntilReset, nextResetTime } = useTokens();
 
   //const WEBHOOK_URL = "https://webhook.vendaseguro.tech/webhook/0fc3496c-5dfa-4772-8661-da71da6353c7";
-  const WEBHOOK_URL = "https://n8n.vendaseguro.tech/webhook-test/0fc3496c-5dfa-4772-8661-da71da6353c7";
+  //const WEBHOOK_URL = "https://n8n.vendaseguro.tech/webhook-test/0fc3496c-5dfa-4772-8661-da71da6353c7";
+  //const WEBHOOK_URL = "https://n8n.srv1137252.hstgr.cloud/webhook/0fc3496c-5dfa-4772-8661-da71da6353c7";
+  /*TESTE*/ const WEBHOOK_URL = "https://n8n.srv1137252.hstgr.cloud/webhook-test/0fc3496c-5dfa-4772-8661-da71da6353c7";
 
   const messagesEndRef = useRef<HTMLDivElement>(null);
   const fileInputRef = useRef<HTMLInputElement>(null);
